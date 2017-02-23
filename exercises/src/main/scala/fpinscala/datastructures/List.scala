5975ce3a84a722da83541afcd26133ae333a4f29
Kate Sickora <katelindsay32@gmail.com>
2017-02-22 20:50:20 -0600

    Lists homework

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,29 +1,52 @@
 package fpinscala.datastructures
 
-sealed trait List[+A] // `List` data type, parameterized on a type, `A`
-case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
+sealed trait List[+A]
+
+// `List` data type, parameterized on a type, `A`
+case object Nil extends List[Nothing]
+
+// A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
 which may be `Nil` or another `Cons`.
  */
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+object List {
+  def main(args: Array[String]): Unit = {
+    //var result = foldRight(List(1, 2, 3), 1)((x, y) => x + y)
+    //var result2 = foldLeft(List(1, 2, 3), 1)((x, y) => x + y)
+    var lengthResult: Int = length(List(1, 2, 3, 4, 5, 6))
+    println(lengthResult)
+
+    var lengthLeftResult: Int = lengthLeft(List(1, 2, 3, 4, 5, 6))
+    println(lengthLeftResult)
+
+    val test = foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _))
+
+    val list = List(1, 2, 3, 4)
+    println(list)
+    val reversed = reverseList(list)
+    println(reversed)
+  }
+
+  // `List` companion object. Contains functions for creating and working with lists.
+  def sum(ints: List[Int]): Int = ints match {
+    // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   }
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
+  val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -34,21 +57,14 @@ object List { // `List` companion object. Contains functions for creating and wo
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
-    }
-
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
-
 
   def tail[A](l: List[A]): List[A] = ???
 
@@ -60,9 +76,41 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def init[A](l: List[A]): List[A] = ???
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = {
+    foldRight(l, 0)((_, acc) => {
+      println(s"acc: $acc")
+      acc + 1
+    })
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def lengthLeft[A](l: List[A]): Int = {
+    foldLeft(l, 0)((acc, _) => {
+      println(s"acc: $acc")
+      acc + 1
+    })
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => {
+        println(s"foldRight($x, $xs) => f($x, foldRight($xs, $z)($f)")
+        f(x, foldRight(xs, z)(f))
+      }
+    }
+
+  @annotation.tailrec
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => {
+      println(s"foldLeft($t, f($z, $h))($f)")
+      foldLeft(t, f(z, h))(f)
+    }
+  }
+
+  def reverseList[A](l: List[A]): List[A] = {
+    foldLeft(l, List[A]())((acc, h) => Cons(h, acc))
+  }
+
+  def map[A, B](l: List[A])(f: A => B): List[B] = ???
 }
